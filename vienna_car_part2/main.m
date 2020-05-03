f8516b5fbdde4888f9676b72ac26baa48fc08913
jamilsonjr <jamilsonpinheiro.jr@gmail.com>
2020-05-03 10:21:10 +0100

    task 1 doing

--- vienna_car_part2/main.m
+++ vienna_car_part2/main.m
@@ -4,34 +4,43 @@
 %%
 % Starting with the stator current:
 %% 
-% $I_s = { V_S \over  Z_S + ({R_R \over S} + j\omega L_R) // ( j \omega L_{Ms})}$
+% $$I_s = { V_S \over  Z_S + ({R_R \over S} + j\omega L_R) //( j \omega
+% L_{Ms})}$$
 %%
-% With respects to the $V_M$ we obtain the following expression with KVL:
-% \begin{equation}
-%    V_{Ms} = V_S - I_S * Z_S
-% \end{equation}
-%
-% -> Then we procede to calculate %I_R% with KCL:
-%
-% \begin{equation}
-%    I_R = I_S - \frac{ V_{Ms} }{ \frac{R_R}{S} + j*\omega * L_R }
-% \end{equation}
-%
-% -> Knowing now the Rotor current we can find the Phase Power on each phase with:
-%
-% \begin{equation}
-%    P = I_R^2 * \frac{ R_R * (S-1) }{ S }
-% \end{equation}
-%
-% -> The Mechanical Power is the sum of the Power in the three fases:
-%
-% \begin{equation}
-%    P_{Mec} = 3 * P
-% \end{equation}
-%
-% -> Finally, the Torque is given by:
-%
-% \begin{equation}
-%    T = \frac{ P_{Mec} }{ \Omega_S }
-% \end{equation}
+% With respects to the $V_{Ms}$ we obtain the following expression with KVL:
+%%
+% $$ V_{Ms} = V_S - I_S * Z_S $$
+%%
+% Then we procede to calculate $I_R$ with KCL:
+%%
+% $$ I_R = I_S - \frac{ V_{Ms} }{ \frac{R_R}{S} + j*\omega * L_R } $$
+%%
+% Knowing now the Rotor current we can find the Phase Power on each phase with:
+%%
+% $$ P = I_R^2 * \frac{ R_R * (S-1) }{ S } $$
+%%
+% The Mechanical Power is the sum of the Power in the three fases:
+%%
+% $$ P_{Mec} = 3 * P $$
+%%
+% Finally, the Torque is given by:
+%%
+% $$ T = \frac{ P_{Mec} }{ \Omega_S } $$
 % 
+%%
+clear
+clc
+close all
+frequency = 0:0.76:76;
+stator_voltage = 0:0.76:76;
+slip = 0:0.01:1;
+slip = fliplr(slip);
+%
+torque_sim = sim('induction_motor');
+figure();
+plot(frequency, abs(torque_sim.torque), 'LineWidth', 1);
+set( gca, 'FontSize', 11);
+grid on;
+title('Torque');
+xlabel('Frequancy $[Hz]$','Interpreter', 'latex');
+ylabel('Torque [$Nm$]','Interpreter', 'latex');
